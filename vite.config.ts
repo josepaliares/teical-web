94c1e8e6e9ee68ebb8b8a65fd0d2a7891e937e28
josepaliares <josearthurpaliares@gmail.com>
2026-03-27 05:00:17 -0300

    fix the github pages not display the website

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: process.env.NODE_ENV === 'production' ? '/teical-web/' : '/',
-})
+  base: command === 'build' ? '/teical-web/' : '/',
+}))
